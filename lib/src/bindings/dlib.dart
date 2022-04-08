62e8920fa35d8c671f3bcd3dd3175da07f69b16d
gujo <gujo@itu.dk>
2022-04-08 12:57:10 +0200

    debug with prints

--- lib/src/bindings/dlib.dart
+++ lib/src/bindings/dlib.dart
@@ -35,9 +35,13 @@ DynamicLibrary tflitelib = () {
   } else if (Platform.isIOS) {
     return DynamicLibrary.process();
   } else {
+    print('pathToBinaries -> $pathToBinaries');
+    print('binaryName -> $binaryName');
     var binaryPath = dart_path.join(pathToBinaries, binaryName); // '$pathToBinaries/$binaryName';
     // var binaryFilePath = Uri(path: binaryPath).toFilePath();
+    print('binaryPath -> $binaryPath');
     var finalPath = dart_path.normalize(binaryPath);
+    print('finalPath -> $finalPath');
     return DynamicLibrary.open(finalPath);
   }
 }();
